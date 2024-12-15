b233d5cc00ad371acc389b85c24e2c0ec9d79430
hilmiatha <hilmiatha2003@gmail.com>
2024-12-15 17:33:18 +0700

    feat: implemented flag

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"flag"
+)
+
 func main () {
+	// Define the command line flags
+	var help bool
+	var inputPath, outputPath string
+
+	// Parse the command line flags
+	flag.StringVar(&inputPath, "i", "", "JSON input file path (shorthand)")
+	flag.StringVar(&inputPath, "input", "", "JSON input file path")
+
+	flag.StringVar(&outputPath, "o", "", "JSON output file path (shorthand)")
+	flag.StringVar(&outputPath, "output", "", "JSON output file path")
+
+	flag.BoolVar(&help, "h", false, "Show help (shorthand)")
+	flag.BoolVar(&help, "help", false, "Show help")
+
+
+	flag.Parse()
+
+
+	// Show help if requested
+	if help || inputPath == "" || outputPath == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+
+
+	
 
 }
+
