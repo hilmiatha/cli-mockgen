44977ffea7cabe9e0e0371710267f92705096cb8
hilmiatha <hilmiatha2003@gmail.com>
2024-12-15 17:59:56 +0700

    data type validation

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 )
-
+var SUPPORTED_MOCK_DATA = map[string]bool{
+	"name": true,
+	"address": true,
+	"phone": true,
+	"date": true,
+}
 func main () {
 	// Define the command line flags
 	var help bool
@@ -26,8 +38,31 @@ func main () {
 	// Show help if requested
 	if help || inputPath == "" || outputPath == "" {
 		flag.PrintDefaults()
-		return
+		os.Exit(0)
 	}
+
+	// Check if the input file exists
+	if err := validateInputFile(inputPath); err != nil {
+		fmt.Printf("Input file does not exist, \n error: %s\n", err)
+		os.Exit(0)
+	}
+
+	// Check if the input file exists
+	if err := validateOutputPath(outputPath); err != nil {
+		fmt.Printf("Output file does not exist, \n error: %s", err)
+	}
+
+	// Open the input file
+	var mapping map[string]string
+	if err := readInput(inputPath, &mapping); err != nil {
+		fmt.Printf("Error reading input file: %s\n", err)
+		os.Exit(0)
+	}
+
+	for k,v := range mapping {
+		fmt.Printf("Key: %s, Value: %s\n", k, v)
+	}
+
 
 
 
@@ -35,3 +70,77 @@ func main () {
 
 }
 
+func validateInputFile (inputPath string) error {
+	if _, err := os.Stat(inputPath); os.IsNotExist(err) {
+		return err
+	}
+	return nil
+}
+
+func validateOutputPath (outputPath string) error {
+	if _, err := os.Stat(outputPath); os.IsNotExist(err) {
+		return err
+	}
+	fmt.Println("Output file already exists, do you want to overwrite it? (y/n)")
+	confirmOverwrite()
+	return nil
+}
+
+func confirmOverwrite () {
+	reader := bufio.NewReader(os.Stdin)
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input")
+	}
+	response = strings.TrimSpace(strings.ToLower(response))
+	if response != "y" && response != "yes"{
+		fmt.Println("Exiting program")
+		os.Exit(0)
+	}
+}
+
+func readInput(inputPath string, mapping *map[string]string) error {
+	if inputPath == "" {
+		return errors.New("input file path is not valid")
+	}
+
+	if mapping == nil {
+		return errors.New("mapping is not valid")
+	}
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	fileByte, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	if len(fileByte) == 0 {
+		return errors.New("input file is empty")
+	}
+
+	// Unmarshal the JSON
+	if err := json.Unmarshal(fileByte, mapping); err != nil {
+		return err
+	}
+
+	if err := validateMapping(mapping); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateMapping(mapping *map[string]string) error {
+	for _,v := range *mapping {
+		if !SUPPORTED_MOCK_DATA[v] {
+			return fmt.Errorf("unsupported mock data type : %s", v)
+		}
+	}
+	return nil
+}
